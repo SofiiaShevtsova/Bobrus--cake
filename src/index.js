aa842f2d772255a09bfe9804d18080d883680b95
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-01-07 22:00:54 +0200

    do start

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { store, persistor } from 'redux/reduxStore';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Chakra } from 'components/theme';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Chakra>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
             <App />
-          </PersistGate>
-        </Provider>
-      </BrowserRouter>
     </Chakra>
   </React.StrictMode>
 );
