947900c168f58148b16f0880a5aaf0722abe4044
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-01-07 23:25:49 +0200

    add style About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,22 @@
-import { Box, Heading, Text } from "@chakra-ui/react"
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 const About = () => {
-    return (<Box minH="200px">
-        <Heading>Приготований вдома</Heading>
-        <Text>Смачні торти тільки з найкращих продуктів. Приготовані в домашніх умовах з любовю.</Text>
-    </Box>)
-}
+  return (
+    <Box minH="200px" p="30px">
+      <Heading
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        bgClip="text"
+        fontSize="6xl"
+        fontWeight="extrabold"
+      >
+        Prepared at home
+      </Heading>
+      <Text color="pink.300">
+        Delicious cakes made only from the best products. Prepared at home with
+        love.
+      </Text>
+    </Box>
+  );
+};
 
-export default About
+export default About;
